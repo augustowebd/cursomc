8834645c09f76d5c577172cc26371c5d2c8e7417
J. Augusto <augustowebd@gmail.com>
2018-07-19 19:40:41 -0300

    Configuração do H2 no projeto

--- src/main/java/com/augustowebd/cursomc/domain/Categoria.java
+++ src/main/java/com/augustowebd/cursomc/domain/Categoria.java
@@ -2,8 +2,17 @@ package com.augustowebd.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
