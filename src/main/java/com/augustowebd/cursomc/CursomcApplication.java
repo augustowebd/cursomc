86670e5ce57dccef2889c3593168a665648c76c5
J. Augusto <augustowebd@gmail.com>
2018-07-20 16:38:10 -0300

    Criando operação de instanciação de Categoria

--- src/main/java/com/augustowebd/cursomc/CursomcApplication.java
+++ src/main/java/com/augustowebd/cursomc/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.augustowebd.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.augustowebd.cursomc.domain.Categoria;
+import com.augustowebd.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository repo; 
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cInfo = new Categoria(null, "Informática");
+		Categoria cEscr = new Categoria(null, "Escritório");
+		
+		repo.saveAll(Arrays.asList(cInfo, cEscr));
 	}
 }
